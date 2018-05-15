b77c208d8a8afc063cad869965147b457fd6aefc
Juslesan <santeri.juslenius@gmail.com>
2018-05-15 13:00:52 +0300

    Heroku

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ app.delete('/courses/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
