fe79ecc572a5a1147fce6635e92192273ac0535e
Juslesan <santeri.juslenius@gmail.com>
2018-05-15 12:55:19 +0300

    Heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 
 let courses = [
