bd2e100644da12555721ffca368f1f0a22fe3064
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-05-18 12:21:21 +0300

    linjakoodit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 let courses
-jsonfile.readFile(fileLocation, function(err, obj) {
+jsonfile.readFile(fileLocation, function (err, obj) {
   courses = obj
 })
 
@@ -39,28 +39,32 @@ const generateId = () => {
   const maxId = courses.length > 0 ? courses.map(n => n.id).sort().reverse()[0] : 1
   return maxId + 1
 }
-
+*/
 app.post('/courses', (request, response) => {
   const body = request.body
+  console.log('request', body)
 
-  if (body.content === undefined) {
+  if (body.name === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
 
   const course = {
+    code: body.code,
     name: body.name,
-    courseId: body.courseId,
-    compulsory: body.compulsory,
     level: body.level,
-    url: body.url,
-    id: generateId()
+    compulsory: body.compulsory,
+    prereqs: body.prereqs,
+    studytrack: body['study track'],
+    ects: body.ects,
+    url: body.url
   }
-
+  console.log('course:' , course)
   courses = courses.concat(course)
-
+  console.log('courses',)
   response.json(course)
 })
 
+/*
 app.delete('/courses/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(course => course.id !== id)
