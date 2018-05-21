ac13509bb9b174a5506a25dfe813d777bac8ceac
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-05-21 15:34:50 +0300

    Test update

--- tests/course_api.test.js
+++ tests/course_api.test.js
@@ -2,7 +2,7 @@ const supertest = require('supertest')
 const {app, server} = require('../src/index.js')
 const api = supertest(app)
 const jsonfile = require('jsonfile')
-const TKT10001 = require('./data/data.js')
+const {TKT10001} = require('./data/data.js')
 
 test('courses are returned as json', async () => {
     await api
@@ -17,7 +17,7 @@ test('with course id correct course is returned', async () => {
         .get('/courses/TKT10001')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-    expect(response.body).toEqual(TKT10001.TKT10001)
+    expect(response.body).toEqual(TKT10001)
 })
 
 afterAll(() => {
