e89c562ab73a1636d6707a161781f4bfd9f6301c
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-05-31 13:27:35 +0300

    env var

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -2,29 +2,25 @@ const express = require('express')
 const loginRouter = express.Router()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const prodPath = './../../resources/accounts'
+const devPath = './../../resources/accounts'
+const testPath = './../../tests/data/testAccounts'
 
-
+const choosePath = () => {
+    return (process.env.NODE_ENV === 'production') ? prodPath : (process.env.NODE_ENV === 'development' ? devPath : testPath)
+}
 
 const validateLogin = async (username, password) => {
-
-    if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
-        const dbUserAccount = require('./../../resources/accounts').accounts.find(account => account.username === username)
-        return (dbUserAccount !== undefined && await bcrypt.compare(password, dbUserAccount.passwordHash))
-    }
-    if (process.env.NODE_ENV === 'test') {
-        const dbUserAccount = require('./../../tests/data/testAccounts').accounts.find(account => account.username === username)
-        return (dbUserAccount !== undefined && await bcrypt.compare(password, dbUserAccount.passwordHash))
-    }
-
+    const account = require(choosePath()).accounts.find(account => account.username === username)
+    return (account !== undefined && await bcrypt.compare(password, account.passwordHash))
 }
 
 loginRouter.post('/login', async (req, res) => {
 
     if (await validateLogin(req.body.username, req.body.password)) {
-
         const token = jwt.sign({ username: req.body.username, }, process.env.SECRET)
-
         res.status(200).send({ token, username: req.body.username })
+        
     } else {
         return res.status(401).send({ error: 'invalid username or password' })
     }
