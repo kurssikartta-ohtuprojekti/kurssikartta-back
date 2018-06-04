7306db19ed98b617f74593bb021c66b84c2e4341
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-06-04 14:06:01 +0300

    test debug

--- tests/course_api.test.js
+++ tests/course_api.test.js
@@ -1,19 +1,18 @@
 const supertest = require('supertest')
-const {app, server} = require('../src/index.js')
+const { app, server } = require('../src/index.js')
 const api = supertest(app)
 const jsonfile = require('jsonfile')
-const {TKT10001} = require('./data/data.js')
+const { TKT10001 } = require('./data/data.js')
 
 test('courses are returned as json', async () => {
-   response = await api
+    const response = await api
         .get('/courses')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-        console.log(response.body)
 })
 
 test('with course id correct course is returned', async () => {
-       response = await api
+    const response = await api
         .get('/courses/TKT10001')
         .expect(200)
         .expect('Content-Type', /application\/json/)
@@ -21,12 +20,12 @@ test('with course id correct course is returned', async () => {
 })
 
 test('with incorrect id status 404 error message is returned', async () => {
-       response = await api
+    response = await api
         .get('/courses/TKT1001')
         .expect(404)
 })
 
 afterAll(() => {
     server.close()
- })
+})
 
