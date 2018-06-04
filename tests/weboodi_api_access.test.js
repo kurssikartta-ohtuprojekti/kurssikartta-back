216f06f5030c55519c1f1ddc6ac4a48ba2e09473
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-06-04 14:48:48 +0300

    test debug

--- tests/weboodi_api_access.test.js
+++ tests/weboodi_api_access.test.js
@@ -1,4 +1,4 @@
-
+/*
 const supertest = require('supertest')
 const courseInfo = require('../src/weboodi/courseInfo')
 const { weboodiApiBase } = require('../src/urls')
@@ -8,3 +8,4 @@ test('with an existing course code data is returned', async () => {
   //  const info = await courseInfo.getCourseInfo(TKT10001.code)
    // console.log('test info', info)
 })
+*/
