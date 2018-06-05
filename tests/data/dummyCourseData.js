f42bf397135139ce6685bfb9aa4647a9d5acf4ed
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-06-05 15:10:09 +0300

    Refaktorointia ja testejä

---
+++ tests/data/dummyCourseData.js
@@ -0,0 +1,27 @@
+const TKT10001 = {
+    "code": "TKT10001",
+    "name": "Johdatus tietojenkäsittelytieteeseen",
+    "level": "Perusopinnot",
+    "compulsory": true,
+    "prereqs": [],
+    "studytrack": [],
+    "ects": "5",
+    "url": "https://courses.helsinki.fi/fi/TKT10001"
+}
+
+const CSM14209 = {
+    "code": "CSM14209",
+    "name": "Seminar in Novel Software Architecture Design",
+    "level": "Syventävät",
+    "compulsory": false,
+    "prereqs": [],
+    "studytrack": [
+        "ss"
+    ],
+    "ects": "5",
+    "url": "https://courses.helsinki.fi/fi/CSM14209"
+}
+
+module.exports = {
+    TKT10001, CSM14209
+}
