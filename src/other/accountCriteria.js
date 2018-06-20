b64c9f44f843b94219e1d37cfdc3faf411d8c312
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-06-20 12:21:52 +0300

    signup update

--- src/other/accountCriteria.js
+++ src/other/accountCriteria.js
@@ -1,10 +1,26 @@
-const MIN_LENGTH = 10
-const MAX_LENGTH = 20
-const REGEXES = [/\d/]
+/* password */
+const PW_MIN_LENGTH = 10
+const PW_MAX_LENGTH = 20
+const PW_REQUIRED_REGEXES = [/\d/] // huom lisää validoinnit 
 
+/* username */
+
+const USERNAME_MIN_LENGTH = 3
+const USERNAME_MAX_LENGTH = 10
+const USERNAME_REQUIRED_REGEXES = [] // huom lisää validoinnit 
+
+
+
+/* cryptography */
+
+const SALT_ROUNDS = 10;
 
 module.exports = {
-    MIN_LENGTH,
-    MAX_LENGTH,
-    REGEXES
+    PW_MIN_LENGTH,
+    PW_MAX_LENGTH,
+    PW_REQUIRED_REGEXES,
+    SALT_ROUNDS,
+    USERNAME_MIN_LENGTH,
+    USERNAME_MAX_LENGTH,
+    USERNAME_REQUIRED_REGEXES
 }
--- src/routers/signupRouter.js
+++ src/routers/signupRouter.js
@@ -4,15 +4,24 @@ const jsonfile = require('jsonfile')
 const jwt = require('jsonwebtoken')
 const paths = require('./../other/paths')
 const messages = require('./../other/messages')
-const passwordCriteria = require('./other/passwordCriteria')
+const accountCriteria = require('./other/passwordCriteria')
+const { getAccount, getNextAccountId } = require('./../utils/accountHandler')
+const bcrypt = require('bcrypt')
 
 
 
 const validatePassword = (password) => {
     return (password !== undefined
-        && password.length >= passwordCriteria.MIN_LENGTH
-        && password.length <= passwordCriteria.MAX_LENGTH
-        && passwordCriteria.REGEXES.every(regex => password.matches(regex)))
+        && password.length >= accountCriteria.PW_MIN_LENGTH
+        && password.length <= accountCriteria.PW_MAX_LENGTH
+        && accountCriteria.PW_REQUIRED_REGEXES.every(regex => password.matches(regex)))
+}
+
+const validateUsername = (username) => {
+    return (username !== undefined
+        && username.length >= accountCriteria.USERNAME_MIN_LENGTH
+        && password.length <= accountCriteria.USERNAME_MIN_LENGTH
+        && accountCriteria.USERNAME_REQUIRED_REGEXES.every(regex => username.matches(regex)))
 }
 
 signupRouter.post('/signup', async (req, res) => {
@@ -20,8 +29,25 @@ signupRouter.post('/signup', async (req, res) => {
     if (req.password === undefined || req.username === undefined) return res.status(400).json({ error: messages.NO_USERNAME_OR_PASSWORD })
 
     const password = req.password
+    const username = req.username
 
     if (!validatePassword(password)) return res.status(400).json({ error: messages.PW_MIN_CRITERIA_NOT_MET })
+    if (!validateUsername(username)) return res.status(400).json({ error: messages.USERNAME_MIN_CRITERIA_NOT_MET })
 
+    // HUOM lisää regexit usernamen validoimiseksi
+    if (getAccount(username)) return { error: messages.USERNAME_TAKEN }
+
+    const passwordHash = await bcrypt.hash(password, accountCriteria.SALT_ROUNDS)
+
+    const newAccount = {
+            id: getNextAccountId(),
+            username: username,
+            passwordHash: passwordHash
+    }
+
+    //jsonfile.writeFile(paths.getAccountJsPath)
+
+    const token = jwt.sign({ username: req.body.username, }, process.env.SECRET)
+    res.status(201).send({ token, username: req.body.username })
 
 })
