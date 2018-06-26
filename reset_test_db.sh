2743261cdd63f99f266a2d3fe2e2014622e1ee42
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-06-26 16:27:11 +0300

    role boolean -> text (admin/user)

--- reset_test_db.sh
+++ reset_test_db.sh
@@ -2,5 +2,5 @@
 
 dropdb kurssikartta_test
 createdb kurssikartta_test
-psql -c 'CREATE TABLE accounts (id SERIAL PRIMARY KEY, username text UNIQUE NOT NULL, passwordhash text NOT NULL, role boolean);' kurssikartta_test
-psql -c "INSERT INTO accounts VALUES (DEFAULT, 'admin', '\$2b\$10\$4tWSs8g8yKlEW0m0dJVnXe.vqNLAiPanrhUv8ekT.TqmNUN4CXnae', true);" kurssikartta_test
+psql -c 'CREATE TABLE accounts (id SERIAL PRIMARY KEY, username text UNIQUE NOT NULL, passwordhash text NOT NULL, role text NOT NULL);' kurssikartta_test
+psql -c "INSERT INTO accounts VALUES (DEFAULT, 'admin', '\$2b\$10\$4tWSs8g8yKlEW0m0dJVnXe.vqNLAiPanrhUv8ekT.TqmNUN4CXnae', 'admin');" kurssikartta_test
