64b640a66960e68cb5d4e1e4cd14d384258cb9c4
Mikko Moisio <mikko.moisio@helsinki.fi>
2018-06-27 18:02:41 +0300

    default magic values for stuff specified in .env

--- src/utils/tokenHandler.js
+++ src/utils/tokenHandler.js
@@ -4,7 +4,8 @@ const jwt = require('jsonwebtoken')
 
 const createToken = (account) => {
   //  console.log('env exp time:', process.env.LOGIN_EXPIRATION_TIME)
-    return jwt.sign({ username: account.username, role: account.role }, process.env.SECRET, {expiresIn: process.env.LOGIN_EXPIRATION_TIME})
+    const expTime = process.env.LOGIN_EXPIRATION_TIME | '2h'
+    return jwt.sign({ username: account.username, role: account.role }, process.env.SECRET, {expiresIn: expTime})
 }
 
 const validateToken = async (token) => {
